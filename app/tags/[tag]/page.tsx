7f3b5658936d296855eed330b18dc09e1f4d4a17
Bran <june20516@gmail.com>
2023-07-12 17:37:51 +0900

    [feature] 태그된 목록 페이지 작성

---
+++ app/tags/[tag]/page.tsx
@@ -0,0 +1,34 @@
+import PostListItem from '@/app/posts/components/PostListItem';
+import { getAllPostList, getAllTags } from '@/lib/staticFileApi';
+import { NextPage } from 'next';
+import Link from 'next/link';
+
+export async function generateStaticParams() {
+  const tags = getAllTags().map(tag => {
+    return { tag: tag };
+  });
+  return tags;
+}
+
+interface TagProps {
+  params: { tag: string };
+}
+
+const Tag: NextPage<TagProps> = ({ params }: TagProps) => {
+  const tag = params.tag;
+  const tagedPosts = getAllPostList().filter(post => post.meta.tags?.includes(tag));
+  return (
+    <>
+      <div>
+        {tag}가 태그된 글 목록 - <span className="italic">{tagedPosts.length}</span>개
+      </div>
+      <ol>
+        {tagedPosts.map((post, index) => (
+          <PostListItem key={index} post={post} hasThumbnail={false} />
+        ))}
+      </ol>
+    </>
+  );
+};
+
+export default Tag;
