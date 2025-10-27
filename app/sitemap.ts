b529d3df3461c61e07ec48dc7cb8ed3c47c1ae4f
Bran <june20516@gmail.com>
2025-10-28 08:47:05 +0900

    hotfix: 사이트맵 path의 인코딩 확장

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,44 +2,63 @@ import { getAllPostList, getAllTags } from '@/lib/staticFileApi';
 import { MetadataRoute } from 'next';
 
 const siteUrl = process.env.HOST_URL;
+const encodeRFC3986 = (str: string) => new URLSearchParams({ k: str }).toString().split('=')[1];
+
+const buildUrl = ({
+  path,
+  params,
+}: {
+  path?: string | string[];
+  params?: Record<string, string>;
+}) => {
+  let url = siteUrl;
+  const pathSegment = Array.isArray(path)
+    ? path.map(encodeRFC3986).join('/')
+    : typeof path === 'string'
+    ? encodeRFC3986(path)
+    : '';
+  const paramSegment = new URLSearchParams(params).toString();
+
+  return [siteUrl, pathSegment, paramSegment].filter(Boolean).join('/');
+};
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const staticRoutes: MetadataRoute.Sitemap = [
     {
-      url: encodeURI(`${siteUrl}/`),
+      url: buildUrl({ path: '' }),
       lastModified: new Date(),
     },
     {
-      url: encodeURI(`${siteUrl}/about`),
+      url: buildUrl({ path: 'about' }),
       lastModified: new Date(),
     },
     {
-      url: encodeURI(`${siteUrl}/posts`),
+      url: buildUrl({ path: 'posts' }),
       lastModified: new Date(),
     },
     {
-      url: encodeURI(`${siteUrl}/tags`),
+      url: buildUrl({ path: 'tags' }),
       lastModified: new Date(),
     },
     {
-      url: encodeURI(`${siteUrl}/lab`),
+      url: buildUrl({ path: 'lab' }),
       lastModified: new Date(),
     },
     {
-      url: encodeURI(`${siteUrl}/lab/space-today`),
+      url: buildUrl({ path: ['lab', 'space-today'] }),
       lastModified: new Date(),
     },
   ];
 
   const posts = getAllPostList();
   const postRoutes: MetadataRoute.Sitemap = posts.map(post => ({
-    url: encodeURI(`${siteUrl}/posts/${post.slug}`),
+    url: buildUrl({ path: ['posts', post.slug] }),
     lastModified: new Date(post.meta.date),
   }));
 
   const tags = getAllTags();
   const tagRoutes: MetadataRoute.Sitemap = tags.map(tag => ({
-    url: encodeURI(`${siteUrl}/tags/${tag}`),
+    url: buildUrl({ path: ['tags', tag] }),
     lastModified: new Date(),
   }));
 
